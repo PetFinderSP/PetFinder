7872f16785514fc11fc346bfcf271721d18c1db7
Lucas-Monteiro07 <lucasmontesilva@gmail.com>
2022-06-23 15:48:12 -0300

    Commit Feature/Models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.forms import ImageField
 
-# Create your models here.
+class Depoimentos(models.Model):
+    imagem_depoimento = models.ImageField()
+    nome_depoimento = models.CharField(max_length=100)
+    texto_depoimento = models.TextField()
+
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('sobre', views.sobre, name='sobre')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
