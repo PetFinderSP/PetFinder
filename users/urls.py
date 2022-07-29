f7d600b3c2ef2b633f85d2eae8f79deb48323a2b
Lucas-Monteiro07 <lucasmontesilva@gmail.com>
2022-07-28 23:10:27 -0300

    Feat Edita user

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('cadastro', views.cadastro, name='cadastro'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    path('dashboard', views.dashboard, name='dashboard')
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('edita_user', views.edita_user, name='edita_user')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
+from re import U
 from django.shortcuts import redirect, render
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.contrib.auth.models import User
 
 
@@ -11,12 +12,15 @@ def cadastro(request):
         senha = request.POST['password']
         senha2 = request.POST['password2']
         if not username.strip():
+            messages.error(request, 'O campo do nome não pode ficar em branco!')
             print('O campo do nome não pode ficar em branco')
             return redirect('cadastro')
         if not email.strip():
+            messages.error(request, 'o campo do email não pode ficar em branco!')
             print('O campo do email não pode ficar em branco')
             return redirect('cadastro')
         if senha != senha2:
+            messages.error(request, 'As senhas não são iguais!')
             print('As senhas não são iguais')
             return redirect('cadastro')
         if User.objects.filter(email=email).exists():
@@ -25,6 +29,7 @@ def cadastro(request):
         else:
             user = User.objects.create_user(username=username, email=email, password=senha2)
             user.save()
+            messages.success(request, 'Usuário Cadastrado com Sucesso')
             print('Usuário Criado com Sucesso!')
             return redirect('login')
     else:
@@ -35,6 +40,7 @@ def login(request):
         username = request.POST['username']
         senha = request.POST['password']
         if username == "" or senha == "":
+            messages.error(request, 'Os campos usuário e senha não podem ficar em branco!')
             print('Os campos email e senha não podem ficar em branco')
             return redirect('login')
         user = auth.authenticate(request, username=username, password=senha)
@@ -43,6 +49,7 @@ def login(request):
             
             return redirect('dashboard')
         else:
+            messages.error(request, 'Usuário ou senha inválidos!')
             print('Usuário ou senha inválidos')
             return redirect('login') 
 
@@ -58,3 +65,35 @@ def dashboard(request):
         return render(request, 'users/dashboard.html')
     else:
         return redirect('index')
+
+def edita_user(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            user_id = request.POST['user_id']
+            u = request.POST['username']
+            e = request.POST['email']
+            senha_nova = request.POST['password2']
+            senha_nova2 = request.POST['password3']
+            myuser = User.objects.get(pk=user_id)
+            if not u.strip():
+                messages.error(request, 'O campo do nome não pode ficar em branco!')
+                print('O campo do nome não pode ficar em branco')
+                return redirect('edita_user')
+            if not e.strip():
+                messages.error(request, 'o campo do email não pode ficar em branco!')
+                print('O campo do email não pode ficar em branco')
+                return redirect('edita_user')
+            if senha_nova != senha_nova2:
+                messages.error(request, 'As senhas não são iguais!')
+                print('As senhas não são iguais')
+                return redirect('edita_user')
+            myuser.username=u
+            myuser.email=e
+            myuser.set_password(senha_nova2)
+            myuser.save()
+            messages.success(request, 'Atualizado com sucesso!')
+            return redirect('login')
+
+        return render(request,'users/edita_user.html')
+    else:
+        return redirect('login')
